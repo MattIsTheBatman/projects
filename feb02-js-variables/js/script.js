840cf01bc5f8cfa55d85818198d9c2711d8357ec
Matthew Dias <Matthew Dias>
2017-02-02 15:45:53 -0800

    feb02

---
+++ feb02-js-variables/js/script.js
@@ -0,0 +1,47 @@
+// var externalJavascript = "wha da heck?";
+var camelCase;
+var not_camel_Case;
+var camel_Case;
+var $dollarCase;
+var dollar$Case;
+var _underscore;
+
+var firstVar;
+
+var variable;
+
+var UppercaseCamel;
+var SUPERUPPERCASE;
+var myNumberObject = 3;
+
+var myNewNumberObject = myNumberObject.toString();
+
+myNumberObject.toString();
+// console.log(myNumberObject);
+// console.log(myNewNumberObject);
+
+var myStringObject = "This is a string object";
+var myOtherStringObject = 'This is also a string object';
+
+console.log(myStringObject, myOtherStringObject);
+
+var myBooleanTrue = false;
+var myBooleanFalse = true;
+
+var myFloatObjects = 3.1;
+
+console.log(0.2 * 0.1);
+
+var myArrayObject = ['red', 8, true, ['r', 'b', 'g']];
+
+var myObjectObject = {
+  make: 'jeep',
+  package: function(){
+      alert(this.make);
+  }
+};
+console.log(myObjectObject);
+
+var a = 5;
+var b = 10;
+var c = a + b;
