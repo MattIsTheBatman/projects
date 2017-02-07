6512fdcf10f55ac7b43c31e941158f78063120a0
Matthew Dias <Matthew Dias>
2017-02-07 15:54:12 -0800

    feb-js-arrays

--- feb07-js-arrays/js/script.js
+++ feb07-js-arrays/js/script.js
@@ -1,1 +1,47 @@
-var students = ['matt', 'david', 'haley', 'marisa', 'micaela', ''];
+// var students = ['matt', 'david', 'haley', 'marisa', 'micaela', ''];
+// var fruits = ['mango', 'strawberry', 'apple', 'banana', 'avocado']
+// console.log(fruits);
+
+// Push - adds to the end of an array
+// fruits.push('dragonfruit');
+// console.log(fruits);
+
+// Pop - removes from the end of an array
+// var lastFruit = fruits.pop();
+// console.log(fruits);
+// console.log(lastFruit);
+
+// Unshift - adds to the front of an array
+ // fruits.unshift('kiwi');
+ // console.log(fruits);
+
+// Shift - removes from the front of an array
+// var firstFruit = fruits.shift();
+// console.log(fruits);
+// console.log(firstFruit);
+
+
+// IndexOf - finds the index of an item in an array
+// var posFruit = fruits.indexOf('avocado');
+// console.log(posFruit);
+
+
+// Splice - remove an item by index number
+// var remFruit = fruits.splice(1, 3);
+// console.log(remFruit);
+// console.log(fruits);
+
+var subjects = ['math'];
+// console.log(subjects);
+
+
+var twoNew = subjects.unshift('english', 'science');
+// console.log(subjects);
+
+var twoLast = subjects.push('art', 'welding');
+// console.log(subjects);
+
+
+var slicedSubject = subjects.slice(3, 4);
+console.log(subjects);
+console.log(slicedSubject);
