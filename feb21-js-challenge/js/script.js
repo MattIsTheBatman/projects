76218f4f589e06cbf3da51a26a47eaf2a2a5a605
Matthew Dias <Matthew Dias>
2017-02-21 15:55:01 -0800

    feb21-js-challenge

---
+++ feb21-js-challenge/js/script.js
@@ -0,0 +1,30 @@
+var btn = document.getElementById('btn');
+var div = document.getElementsByTagName('div');
+var classes = document.getElementsByClassName('class');
+
+
+var lis = document.querySelectorAll('li');
+var ul = document.querySelector('ul');
+
+// div.addEventListener('mouseover', function(){
+//   alert('hovering the div');
+// });
+
+// for(var i = 0; i < div.length; i++){
+//   div[i].addEventListener('mouseover', function(){
+//     alert('hovering the div');
+//   });
+// }
+
+for(var j = 0; j < classes.length; i++)
+
+// btn.addEventListener('click', function(){
+//    var userName = prompt('Whats your name?').trim();
+//    if(userName.trim() === ''){
+//      alert('not just blanks please')
+//    }else if(userName.length >= 5){
+//       alert('Thanks fam');
+//    }else{
+//      alert('Try again boyo');
+//    }
+// });
